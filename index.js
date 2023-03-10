84443fba37614e9864669466702f2b099b19da80
Andrew Talanov <andrewtalanov@mail.ru>
2023-03-10 14:15:40 +0500

    .

---
+++ index.js
@@ -0,0 +1,46 @@
+"use strict";
+
+function draw(array) {
+
+    let linksHTML = '';
+
+    array.forEach(el => {
+        linksHTML += `<div class="list-element">
+                        <img src="${el.owner.avatar_url}" alt="Аватар аккаунта">
+                        <div class="element-info">
+                            <p>Ссылка на репозиторий: <a href=${el.html_url} target="_blank">${el.name}</a></p>
+                            <p>Имя аккаунта: ${el.owner.login}</p>
+                            <p>Используемый язык: ${el.language ?? 'Не используется'}</p>
+                        </div>
+                    </div>`;
+    });
+    document.querySelector('.message').innerHTML = '';
+    list.innerHTML = linksHTML;
+}
+
+const list = document.querySelector('.list');
+
+document.querySelector('form').addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const searchString = document.querySelector('input').value;
+    const countLinks = 10;
+
+    if (searchString.length < 3) {
+        document.querySelector('.message').innerHTML = 'Введите не менее 3х символов';
+        return;
+    }
+
+    document.querySelector('.message').innerHTML = 'Загрузка...';
+    const url = `https://api.github.com/search/repositories?q=${searchString}&per_page=${countLinks}`;
+
+    fetch(url)
+        .then(response => response.json())
+        .then(response => {
+            console.log(response)
+            draw(response.items);
+        })
+        .catch(error => console.log(error));
+});
+
+
